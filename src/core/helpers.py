609b513608b719788ffe5210012cd244f8837b72
Juan Sobalvarro <sobalvarrog.juans@gmail.com>
2025-12-08 21:24:46 -0600

    feat: added create csv helper

--- src/core/helpers.py
+++ src/core/helpers.py
@@ -1,7 +1,9 @@
 from zipfile import ZipFile
 import os
+import csv
 import pathlib
 from typing import List
+
 
 
 def unzip_and_getfilepaths(zip_file: pathlib.Path, extract_to: pathlib.Path) -> List[pathlib.Path]:
@@ -17,3 +19,14 @@ def unzip_and_getfilepaths(zip_file: pathlib.Path, extract_to: pathlib.Path) -> 
             extracted_files.append(extract_to / file_info.filename)
 
     return extracted_files
+
+def create_csv(data: List[List[str]], output_path: pathlib.Path) -> None:
+    """
+    Creates a CSV file from the given data at the specified output path.
+    Each item in data represents a row in the CSV file.
+    """
+
+    # write data to csv
+    with open(output_path, mode='w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerows(data)
